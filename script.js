5808a6b7a41be51321b6b40bfa885a6f76daa003
raflyfrmnsyh <raflyfirmansyah017@gmail.com>
2023-05-23 23:34:00 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let ImgList = document.querySelectorAll('.box__image-list');
+
+function changePhoto()
+{
+
+    for (let i = 0; i < ImgList.length; i++) {
+        const element = ImgList[i];
+    
+        // console.log(e)
+
+        if (element.classList.contains == "active")
+        {
+            console.log(element)
+        }
+    }
+    
+}
+
+
